7214a030c6875f01c6908d4e6c8bc48c88856994
RomanDerkach <derkachr@gmail.com>
2017-12-15 08:33:53 +0200

    pep

--- api/web.go
+++ api/web.go
@@ -1,55 +1,52 @@
 package api
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
-    "path"
-    // "github.com/gorilla/mux"
-    "github.com/RomanDerkach/homework/storage"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path"
+	// "github.com/gorilla/mux"
+	"github.com/RomanDerkach/homework/storage"
 )
 
-type Book struct{
-    Id string
-    Title string
-    Ganres []string
-    Pages int
-    Price float64
+type Book struct {
+	ID     string
+	Title  string
+	Ganres []string
+	Pages  int
+	Price  float64
 }
 
-
-
 func Server() {
-    http.HandleFunc("/books", BooksHandler)
-    http.HandleFunc("/books/", BooksHandlerByID)
-    http.ListenAndServe(":8081", nil)
+	http.HandleFunc("/books", BooksHandler)
+	http.HandleFunc("/books/", BooksHandlerByID)
+	http.ListenAndServe(":8081", nil)
 }
 
 func BooksHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "POST" {
-        fmt.Println("AAA")
-    } else {
-        jsonBody := storage.GetBooksData()
-        w.Write(jsonBody)
-    }
+	if r.Method == "POST" {
+		fmt.Println("AAA")
+	} else {
+		jsonBody := storage.GetBooksData()
+		w.Write(jsonBody)
+	}
 }
 
-
-func BooksHandlerByID(w http.ResponseWriter, r *http.Request){
-    data := storage.GetBooksData()
-    books := []Book{}
-    result := Book{}
-    json.Unmarshal(data, &books)
-    fmt.Println(books[0].Id)
-    for _, book := range books{
-        if book.Id == path.Base(r.URL.Path){
-            result = book
-        }
-    }
-    jsonBody, err := json.Marshal(result)
-    if err != nil {
-         http.Error(w, "Error converting results to json",
-         http.StatusInternalServerError)
-    }
-    w.Write(jsonBody)
+func BooksHandlerByID(w http.ResponseWriter, r *http.Request) {
+	data := storage.GetBooksData()
+	books := []Book{}
+	result := Book{}
+	json.Unmarshal(data, &books)
+	fmt.Println(books[0].ID)
+	for _, book := range books {
+		if book.ID == path.Base(r.URL.Path) {
+			result = book
+		}
+	}
+	jsonBody, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, "Error converting results to json",
+			http.StatusInternalServerError)
+	}
+	w.Write(jsonBody)
 }
